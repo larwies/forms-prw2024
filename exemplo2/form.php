ffa82bab02e508f8febb3b210ae04f7aaaa443cc
larwies <farquetelarissa@gmail.com>
2024-06-18 11:40:58 -0300

    ex2

---
+++ exemplo2/form.php
@@ -0,0 +1,29 @@
+<?php 
+    echo "<h2>Tempo Viagem</h2>";
+
+    //$_GET['nome_campo']
+    //$_POST['nome_campo']
+
+
+    //recebendo dados enviados do form html via post 
+
+     $vm = $_GET['vm'];
+     $dv = $_GET['dv'];
+     $cm = $_GET['cm'];
+
+     echo "<h3>Velocidade Média :</h3>";
+     echo $vm;
+
+     echo "<h3>Distância da viagem:</h3>";
+     echo $dv;
+
+     echo "<h3>Consumo médio do veículo:</h3>";
+     echo $cm;
+
+
+$ho = number_format($dv/$vm,2,".","");
+$cm = number_format($dv/$cm,2,".","");
+
+echo "<h3>Total horas: <h3>$ho\n";
+echo "<h3>Total combustivel: <h3>$cm\n";
+?>
